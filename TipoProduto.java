5b52d732839544f8466d672293fa1b1369a3b78c
Lorena <thalitalorena031@gmail.com>
2026-04-20 09:57:20 -0300

    Update TipoProduto.java

--- TipoProduto.java
+++ TipoProduto.java
@@ -1,10 +1,10 @@
 public class TipoProduto {
     private int idTipo;
     private String nome;
-    // private FormaDeVenda formaVenda;
+    private FormaDeVenda formaVenda;
 
     public String getNome() {
-    if (!nome.isEmpty()) {
+    if (nome == null || nome.isEmpty()) { // se nome está vazio, produto é inválido
         return "Nome de produto inválido";
     }
     return nome;
