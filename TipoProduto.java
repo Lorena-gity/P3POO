2dd75ae0c8fc67cb60f7a2c88068275e9b7b638a
Lorena-gity <thalitalorena031@gmail.com>
2026-04-21 15:28:56 -0300

    Desenvolvimento das classes relacionadas a produto

---
+++ TipoProduto.java
@@ -0,0 +1,33 @@
+public class TipoProduto {
+    private int idTipo;
+    private String nome;
+    private FormaDeVenda formaVenda;
+
+    public int getIdTipo() {
+        return idTipo;
+    }
+   
+    public String getNome() {
+        return nome;
+    }
+
+    public FormaDeVenda getFormaDeVenda() {
+        return formaVenda;
+    }
+
+    public void setNome(String nome) {
+        if (nome == null || nome.isEmpty()) {
+            System.out.println("Nome inválido");
+        } else {
+            this.nome = nome;
+        }
+    }
+
+    public void setFormaVenda(FormaDeVenda formaVenda) {
+        if (formaVenda == null) {
+            this.formaVenda = FormaDeVenda.UNIDADE;
+        } else {
+            this.formaVenda = formaVenda;
+        }
+    }
+}
